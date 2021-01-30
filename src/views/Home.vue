a19734363d25ffdd4089be4d2b960ec7cbaf266b
Lewis Luo <ts03002215@gmail.com>
2021-01-31 03:32:13 +0800

    feat: hover, pagination added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,25 +5,31 @@
       <!-- Render cards -->
       <v-row>
         <v-col v-for="item in list" :key="item.name" col="12" sm="4">
-          <v-card class="mx-auto" max-width="344" outlined>
-            <v-list-item three-line>
-              <v-list-item-content>
-                <div class="overline mb-4">
-                  {{ item.name }}
-                </div>
-                <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
-                <v-list-item-subtitle>
-                  Greyhound divisely hello coldly fonwderfully
-                </v-list-item-subtitle>
-              </v-list-item-content>
-              <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
-            </v-list-item>
-            <v-card-actions>
-              <v-btn outlined rounded text>Button</v-btn>
-            </v-card-actions>
-          </v-card>
+          <v-hover v-slot="{ hover }">
+            <v-card class="mx-auto" max-width="344" outlined>
+              <v-list-item v-if="hover" class="show-on-hover" three-line>
+                <v-list-item-content>
+                  <v-list-item-title class="headline mb-1">{{ item.name }}</v-list-item-title>
+                  <v-list-item-subtitle>ok</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item v-if="!hover" class="show-default" three-line>
+                <v-list-item-content>
+                  <v-list-item-title class="headline mb-1">{{ item.name }}</v-list-item-title>
+                  <v-list-item-subtitle>
+                    Greyhound divisely hello coldly fonwderfully
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+                <v-list-item-avatar size="80" color="grey"></v-list-item-avatar>
+              </v-list-item>
+              <v-card-actions>
+                <v-btn outlined rounded text>Button</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-hover>
         </v-col>
       </v-row>
+      <v-pagination v-model="page" :length="list.length/3" :total-visible="5" circle></v-pagination>
     </v-container>
   </div>
 </template>
