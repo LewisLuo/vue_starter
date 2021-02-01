dbc2ef12eb0153729980288d271188da1e61d0ea
Lewis Luo <ts03002215@gmail.com>
2021-02-01 17:25:17 +0800

    feat: add display mode and switch icon

---
+++ src/data.js
@@ -0,0 +1,10 @@
+const data = [
+  { name: 'One' },
+  { name: 'Two' },
+  { name: 'Three' },
+  { name: 'Four' },
+  { name: 'Five' },
+  { name: 'Six' },
+];
+
+module.exports = data;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,38 +2,51 @@
   <div class="home">
     <!-- TODO -->
     <v-container>
-      <!-- Render cards -->
-      <v-row>
-        <v-col v-for="item in showCards" :key="item.name" col="12" sm="4">
-          <v-hover v-slot="{ hover }">
-            <v-card class="mx-auto" max-width="344" outlined>
-              <v-list-item v-if="hover" class="show-on-hover" three-line>
-                <v-list-item-content>
-                  <v-list-item-title class="headline mb-1">{{ item.name }}</v-list-item-title>
-                  <v-list-item-subtitle>ok</v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-              <v-list-item v-if="!hover" class="show-default" three-line>
-                <v-list-item-content>
-                  <v-list-item-title class="headline mb-1">{{ item.name }}</v-list-item-title>
-                  <v-list-item-subtitle>This is a sample card.</v-list-item-subtitle>
-                </v-list-item-content>
-                <v-list-item-avatar size="80" color="grey"></v-list-item-avatar>
-              </v-list-item>
-              <v-card-actions>
-                <v-btn outlined rounded text>Button</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-hover>
-        </v-col>
-      </v-row>
-      <v-pagination
-        v-model="page"
-        :length="list.length / 3"
-        :total-visible="5"
-        @input="changePage"
-        circle
-      ></v-pagination>
+      <!-- Render page -->
+      <div class="icon-select d-flex justify-end pr-2 py-4">
+        <v-btn class="mr-2" @click="displayMode = 'card'" fab>
+          <v-icon>mdi-checkerboard</v-icon>
+        </v-btn>
+        <v-btn @click="displayMode = 'list'" fab>
+          <v-icon>mdi-format-list-bulleted</v-icon>
+        </v-btn>
+      </div>
+      <v-divider class="py-4"></v-divider>
+      <div class="display-card" v-if="displayMode === 'card'">
+        <v-row>
+          <v-col v-for="item in showCards" :key="item.name" col="12" sm="4">
+            <v-hover v-slot="{ hover }">
+              <v-card class="mx-auto" max-width="344" outlined>
+                <v-list-item v-if="hover" class="show-on-hover" three-line>
+                  <v-list-item-content>
+                    <v-list-item-title class="headline mb-1">{{ item.name }}</v-list-item-title>
+                    <v-list-item-subtitle>You are hovering this card.</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-list-item v-if="!hover" class="show-default" three-line>
+                  <v-list-item-content>
+                    <v-list-item-title class="headline mb-1">{{ item.name }}</v-list-item-title>
+                    <v-list-item-subtitle>This is a sample card.</v-list-item-subtitle>
+                  </v-list-item-content>
+                  <v-list-item-avatar size="80" color="grey"></v-list-item-avatar>
+                </v-list-item>
+                <v-card-actions>
+                  <v-btn color="pink" outlined rounded>Petting</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-hover>
+          </v-col>
+        </v-row>
+        <v-pagination
+          v-if="cardList.length > cardsPerPage"
+          v-model="page"
+          :length="Math.ceil(cardList.length / cardsPerPage)"
+          :total-visible="5"
+          @input="changePage"
+          circle
+        ></v-pagination>
+      </div>
+      <div class="display-list" v-if="displayMode === 'list'"></div>
     </v-container>
   </div>
 </template>
@@ -42,26 +55,21 @@
 import Vue from 'vue';
 
 import extensions from '@/mixins/extensions';
+import dataBase from '@/data';
 
 export default Vue.extend({
   name: 'Home',
   mixins: [extensions],
   data: () => ({
-    list: [
-      { name: 'One' },
-      { name: 'Two' },
-      { name: 'Three' },
-      { name: 'Four' },
-      { name: 'Five' },
-      { name: 'Six' },
-    ],
+    cardList: dataBase,
     currentPage: 1,
+    cardsPerPage: 3,
+    displayMode: 'card',
   }),
   computed: {
     showCards() {
-      const cardsPerPage = 3;
-      const startIndex = cardsPerPage * (this.currentPage - 1);
-      const shownCards = this.list.slice(startIndex, startIndex + cardsPerPage);
+      const startIndex = this.cardsPerPage * (this.currentPage - 1);
+      const shownCards = this.cardList.slice(startIndex, startIndex + this.cardsPerPage);
       return shownCards;
     },
   },
